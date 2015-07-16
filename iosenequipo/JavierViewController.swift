9b011fdf40dc03a57d56764af8e2b0fb7123397e
ciceroot <ciceroot@gmail.com>
2015-07-16 09:54:29 +0200

    comentario en javier

--- iosenequipo/JavierViewController.swift
+++ iosenequipo/JavierViewController.swift
@@ -13,7 +13,7 @@ class JavierViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       //esto es una prueba , kike
     }
 
     override func didReceiveMemoryWarning() {
