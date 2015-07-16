8c348ba3fd0150173b9bd8e311c2ed5c782b293e
Hugo <Hugo@08FormadorMac.local>
2015-07-16 10:00:01 +0200

    new VC hugo

---
+++ iosenequipo/HugoViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HugoViewController.swift
+//  iosenequipo
+//
+//  Created by Hugo on 16/7/15.
+//  Copyright (c) 2015 cice. All rights reserved.
+//
+
+import UIKit
+
+class HugoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+
+}
